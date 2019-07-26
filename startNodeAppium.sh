cd1770406ad8bdc61574f86013d72e04498dd32f
qps-mac07 <build@qps-mac07s-Mac-Pro.local>
2019-07-26 23:41:29 +0300

    reused the same appium startup script for simulators and physical devices

--- startNodeAppium.sh
+++ startNodeAppium.sh
@@ -12,14 +12,8 @@ ${selenium_home}/configgen.sh $udid > configs/$udid.json
 
 
 newWDA=false
-#echo type: $type
-if [ "$type" == "simulator" ]; then
-        newWDA=true
-fi
-
-echo Starting appium: ${name} device...
-nohup node ${appium_home}/build/lib/main.js -p ${appium_port} --log-timestamp \
+#TODO: investigate if tablet should be registered separately, what about tvOS
+nohup node ${appium_home}/build/lib/main.js -p ${appium_port} --log-timestamp --device-name "${name}" --automation-name=XCUItest --udid $udid \
   --default-capabilities \
-  '{"mjpegServerPort": '${mjpeg_port}', "webkitDebugProxyPort": '${iwdp_port}', "clearSystemFiles": "false", "preventWDAAttachments": "true", "simpleIsVisibleCheck": "true", "wdaLocalPort": "'$wda_port'", "usePrebuiltWDA": "true", "useNewWDA": "'$newWDA'", "platformVersion": "'$os_version'", "automationName":"'${automation_name}'", "deviceName":"'$name'" }' \
+  '{"mjpegServerPort": '${mjpeg_port}', "webkitDebugProxyPort": '${iwdp_port}', "clearSystemFiles": "false", "webDriverAgentUrl":"'http://${device_ip}:${wda_port}'", "preventWDAAttachments": "true", "simpleIsVisibleCheck": "true", "wdaLocalPort": "'$wda_port'", "usePrebuiltWDA": "true", "useNewWDA": "'$newWDA'", "platformVersion": "'$os_version'", "automationName":"'${automation_name}'", "deviceName":"'$name'" }' \
    --nodeconfig ./configs/$udid.json > "./logs/${name}_${appium_port}.log" 2>&1 &
-
