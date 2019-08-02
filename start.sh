bb3d455785f274a589710806a5603cdb1139b403
vdelendik <vdelendik@qaprosoft.com>
2019-08-02 11:53:41 +0300

    removed useless comments

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-
-# to cauto clean selenoid/appium video crontab is configured to remove mp4 older than 24hrs
-#0 * * * * find $HOME/tools/stage-infra/selenoid/video -mindepth 1 -maxdepth 1 -mmin +1440 -name '*.mp4' | xargs rm -rf
-
-#TODO: add configuration step to share  selenoid/video correctly when cm is used
-#ln -s /home/build/.aerokube/selenoid/video /home/build/tools/stage-infra/selenoid/
 docker network create mcloud
 
 #-------------- START EVERYTHING ------------------------------
