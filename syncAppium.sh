a85e6c38bb32a5dc928286fa00357571f7964294
build <build@builds-Mac-mini.local>
2019-11-13 17:24:28 +0300

    improved sync appium scripts

--- syncAppium.sh
+++ syncAppium.sh
@@ -7,6 +7,7 @@ date +"%T"
 while read -r line
 do
  	udid=`echo $line | cut -d '|' -f ${udid_position}`
+	. ${selenium_home}/getDeviceArgs.sh $udid
         #to trim spaces around. Do not remove!
 	udid=$(echo $udid)
 	if [ "$udid" = "UDID" ]; then
@@ -32,14 +33,21 @@ do
 	fi
 
 	device=`/usr/local/bin/ios-deploy -c -t 1 | grep $udid`
-        if [[ -n "$device" &&  -z "$appium" ]]; then
-		echo "Starting node: ${udid}"
+	#wda check is only for approach with syncWda.sh and usePrebuildWda=true
+	wda=`/usr/bin/curl http://${device_ip}:${wda_port}/status --connect-timeout 2 | grep success`
+        if [[ -n "$device" && -z "$wda" && -n "$appium" ]]; then
+           	echo "WDA is not started or possible crashed for device udid : ${udid} - device name : ${name} . Appium will be stopped till WDA is down."
+		${selenium_home}/stopNodeAppium.sh $udid
+        elif [[ -n "$device" && -n "$wda" && -z "$appium" ]]; then
+		echo "WDA has been started. Starting appium node: ${udid} - device name : ${name}"
                 ${selenium_home}/startNodeAppium.sh $udid
+	elif [[ -n "$device" && -z "$wda" ]]; then
+                echo "WDA is not running. Waiting WDA for : ${udid} - device name : ${name}"
         elif [[ -z "$device" &&  -n "$appium" ]]; then
-		echo "The node will be stopped: ${udid}"
+		echo "The node will be stopped: ${udid} - device name : ${name}"
                 ${selenium_home}/stopNodeAppium.sh $udid
         else
-        	echo "Nothing to do for ${udid}"
+        	echo "Nothing to do for ${udid} - device name : ${name}"
         fi
 done < ${devices}
 echo "Script finished"
