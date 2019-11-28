152c6893a0fa36e13d95595f4c62ec0f130aa3cc
qps-mac07 <build@qps-mac07s-Mac-Pro.local>
2019-11-28 18:34:51 +0300

    return syncAppium.sh consolidated script

---
+++ syncAppium.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+. ${BASEDIR}/set_selenium_properties.sh
+
+echo "Sync Appium script started"
+date +"%T"
+
+logFile=${metaDataFolder}/connectedDevices4Appium.txt
+/usr/local/bin/ios-deploy -c -t 10 > ${logFile}
+
+while read -r line
+do
+ 	udid=`echo $line | cut -d '|' -f ${udid_position}`
+        #to trim spaces around. Do not remove!
+	udid=$(echo $udid)
+	if [[ "$udid" = "UDID" ]]; then
+            continue
+        fi
+        simulator=`echo $line | grep simul`
+        . ${selenium_home}/getDeviceArgs.sh $udid
+
+        appium=`/usr/bin/curl -s --max-time 30 --connect-timeout 30 http://localhost:${appium_port}/wd/hub/status | grep version`
+#	appium=`ps -ef | grep $appium_home/build/lib/main.js  | grep $udid`
+#	echo appium: $appium
+
+	if [[ -n "$simulator" ]]; then
+		device=${name}
+	else
+	        device=`cat ${logFile} | grep $udid`
+	fi
+#	echo device: $device
+
+        if [[ -n "$device" && -z "$appium" ]]; then
+		echo "Starting appium node: ${udid} - device name : ${name}"
+                ${selenium_home}/startNodeAppium.sh $udid
+        elif [[ -z "$device" &&  -n "$appium" ]]; then
+		echo "Appium will be stopped: ${udid} - device name : ${name}"
+                ${selenium_home}/stopNodeAppium.sh $udid
+        else
+        	echo "Nothing to do for ${udid} - device name : ${name}"
+        fi
+done < ${devices}
+echo "Script finished"
+date +"%T"
