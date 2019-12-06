d3b6c7a2101b81eba0cfc333500d98860268084d
qps-mac07 <build@qps-mac07s-Mac-Pro.local>
2019-12-07 01:24:50 +0300

    concatenate start WDA logs

--- startNodeWDA.sh
+++ startNodeWDA.sh
@@ -10,5 +10,5 @@ BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 echo Starting WDA: ${name}, udid: ${udid}, wda_port: ${wda_port}, mjpeg_port: ${mjpeg_port}
 nohup /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -project ${appium_home}/node_modules/appium-webdriveragent/WebDriverAgent.xcodeproj \
-      -scheme WebDriverAgentRunner -destination id=$udid USE_PORT=$wda_port MJPEG_SERVER_PORT=$mjpeg_port test > "${BASEDIR}/logs/${name}_wda.log" 2>&1 &
+      -scheme WebDriverAgentRunner -destination id=$udid USE_PORT=$wda_port MJPEG_SERVER_PORT=$mjpeg_port test >> "${BASEDIR}/logs/${name}_wda.log" 2>&1 &
 
