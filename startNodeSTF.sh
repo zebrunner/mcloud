608c5e5e98c60a70ca5eb3f141078e15abe83bbc
Alexandr Malinovsky <smalinovsky611@gmail.com>
2020-01-29 16:22:51 +0200

    replaced stf_public_node_host to STF_PRIVATE_NODE_HOST in --screen-ws-url-pattern property

--- startNodeSTF.sh
+++ startNodeSTF.sh
@@ -24,7 +24,7 @@ export WEB_PROTOCOL=https
 nohup node /Users/build/tools/stf/lib/cli ios-device --serial ${udid} \
         --provider ${PROVIDER_NAME} --screen-port ${stf_screen_port} --connect-port ${mjpeg_port} --public-ip ${STF_PUBLIC_HOST} --group-timeout 3600 \
         --storage-url ${WEB_PROTOCOL}://${STF_PUBLIC_HOST}/ --screen-jpeg-quality 40 --screen-ping-interval 30000 \
-	--screen-ws-url-pattern ${WEBSOCKET_PROTOCOL}://${STF_PUBLIC_HOST}/d/${stf_public_node_host}/${udid}/${stf_screen_port}/ \
+	--screen-ws-url-pattern ${WEBSOCKET_PROTOCOL}://${STF_PUBLIC_HOST}/d/${STF_PRIVATE_NODE_HOST}/${udid}/${stf_screen_port}/ \
         --boot-complete-timeout 60000 --mute-master never \
         --connect-app-dealer tcp://${STF_PRIVATE_HOST}:7160 --connect-dev-dealer tcp://${STF_PRIVATE_HOST}:7260 \
         --wda-host ${device_ip} \
