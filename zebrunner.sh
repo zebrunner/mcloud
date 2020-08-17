fceb4be9a46ea1eda333499c0eb06610668bc451
vdelendik <vdelendik@zebrunner.com>
2020-08-17 20:39:16 +0000

    simplified zebrunner installer detection

--- zebrunner.sh
+++ zebrunner.sh
@@ -108,7 +108,7 @@ cd ${BASEDIR}
 
 case "$1" in
     setup)
-        if [[ ! -z $ZBR_PROTOCOL || ! -z $ZBR_HOSTNAME || ! -z $ZBR_PORT ]]; then
+        if [[ $ZBR_INSTALLER -eq 1 ]]; then
           setup
         else
           echo_warning "Setup procedure is supported only as part of Zebrunner Server (Community Edition)!"
